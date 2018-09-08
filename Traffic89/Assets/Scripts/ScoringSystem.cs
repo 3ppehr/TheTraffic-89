b0e17c7b88dad749cb8d9ee0e06c1e331a14d461
3ppehr <36326467+3ppehr@users.noreply.github.com>
2018-09-09 03:26:37 +0430

    beta score

---
+++ Traffic89/Assets/Scripts/ScoringSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour {
+    public float myTimer;
+    public Text Score;
+    public float myScore=0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myTimer = Time.deltaTime + myTimer;
+        if(myTimer>=1)
+        {
+            myTimer = 0;
+            myScore += 10;
+            Score.text = myScore.ToString();
+        }
+
+	}
+}
